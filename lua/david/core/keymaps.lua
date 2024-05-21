83754360ce0e26f02fd4ee77bf0b9b0045b91e07
David Martins <contato@davidmartins.net>
2024-05-21 09:57:35 -0300

    Better keymap and added a game to practice vim motions

--- lua/david/core/keymaps.lua
+++ lua/david/core/keymaps.lua
@@ -6,6 +6,14 @@ keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
+-- optimize half page jumping
+keymap.set("n", "<C-d>", "<C-d>zz")
+keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- optimize navigation in the search
+keymap.set("n", "n", "nzz")
+keymap.set("n", "N", "Nzz")
+
 -- increment/decrement numbers
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
