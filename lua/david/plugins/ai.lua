d8dc5a489c546f9391711254974285b16586f6b2
David Martins <contato@davidmartins.net>
2025-03-11 10:52:38 -0300

    Add some shortcuts for codecompanion

--- lua/david/plugins/ai.lua
+++ lua/david/plugins/ai.lua
@@ -1,12 +1,13 @@
 return {
   "olimorris/codecompanion.nvim",
-  config = true,
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
   },
   config = function()
     local codecompanion = require("codecompanion")
+    local keymap = vim.keymap -- for conciseness
+
     codecompanion.setup({
       strategies = {
         chat = {
@@ -17,5 +18,8 @@ return {
         },
       },
     })
+
+    keymap.set("n", "<leader>cc", "<cmd>CodeCompanionChat<CR>", { desc = "Opens code companion chat" })
+    keymap.set("n", "<leader>co", "<cmd>CodeCompanionActions<CR>", { desc = "Opens code companion actions" })
   end,
 }
