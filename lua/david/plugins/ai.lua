96597672eda3586ef4370f90df0eda8b06eccff1
David Martins <contato@davidmartins.net>
2025-03-12 14:31:56 -0300

    Add more shotcuts to codecompanion

--- lua/david/plugins/ai.lua
+++ lua/david/plugins/ai.lua
@@ -19,7 +19,16 @@ return {
       },
     })
 
-    keymap.set("n", "<leader>cc", "<cmd>CodeCompanionChat<CR>", { desc = "Opens code companion chat" })
+    keymap.set("n", "<leader>cc", "<cmd>CodeCompanionChat Toggle<CR>", { desc = "Opens code companion chat" })
     keymap.set("n", "<leader>co", "<cmd>CodeCompanionActions<CR>", { desc = "Opens code companion actions" })
+    keymap.set(
+      "v",
+      "ga",
+      "<cmd>CodeCompanionChat Add<cr>",
+      { noremap = true, silent = true, desc = "Add selected text to the current Chat" }
+    )
+
+    -- Expand 'cc' into 'CodeCompanion' in the command line
+    vim.cmd([[cab cc CodeCompanion]])
   end,
 }
